dcdc31a95e9a2f88abf75734412cdebd7734f854
Shawn <shawntan2008@gmail.com>
2020-06-17 19:04:27 -0500

    Added tip calculation logic and implemented button to call it. Additionally, a minor error check was added when user inputs number of people <= 0.

--- app/src/main/java/com/example/simpletip/MainActivity.kt
+++ app/src/main/java/com/example/simpletip/MainActivity.kt
@@ -5,33 +5,37 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.simpletip.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.activity_main.view.bill_value
 
 private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val viewModel = ViewModelProvider(this).get(TipViewModel::class.java)
 
         binding.lifecycleOwner = this
         binding.tipViewModel = viewModel
 
-// TODO: implement observers for tipViewModel members
-        viewModel.billValue.observe(this, Observer {
-
-        })
-        viewModel.tipPercent.observe(this, Observer {
-
-        })
-        viewModel.numPeople.observe(this, Observer {
-
-        })
+        binding.calculateBtn.setOnClickListener(){
+            if(binding.numPeopleValue.text.toString().toInt() <= 0){
+                Toast.makeText(this, "# Of People must be at least 1", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                viewModel.billValue.value = binding.billValue.text.toString().toDouble()
+                viewModel.tipPercent.value = binding.tipPercentValue.text.toString().toDouble()
+                viewModel.numPeople.value = binding.numPeopleValue.text.toString().toInt()
+                viewModel.calculateValues()
+            }
+        }
 
     }
 }
